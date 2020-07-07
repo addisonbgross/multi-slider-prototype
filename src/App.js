2535c6a832f518881678f5c4cd13b2882fb874dd
addisonbgross <addisonbgross@gmail.com>
2020-07-07 16:54:08 -0400

    chore: set up tooling

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MultiSlider from './components/MultiSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <MultiSlider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MultiSlider/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default function MultiSlider() {
+  return (
+    <div>
+      <Button>Yes</Button>
+    </div>
+  );
+}
